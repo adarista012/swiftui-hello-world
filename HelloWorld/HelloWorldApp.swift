d1bd1df18d6707fd5939bbb5bde485359f9d9df3
antonio arista <ad.arista@acad.ucb.edu.bo>
2023-04-24 11:35:47 -0400

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by antonio dante arista rivas on 24/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
